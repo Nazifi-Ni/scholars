07211e6b6cc0e17bb0bdd077873087d3b088bfec
Nazifi Ibrahim <nazifiibrahim003@gmail.com>
2026-07-27 00:41:21 +0100

    feat: replace multitag with standalone monetag vignette banner script (zone 11424905) for clean ad serving without popunders

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -131,7 +131,8 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
-        <script src="https://quge5.com/88/tag.min.js" data-zone="264153" async data-cfasync="false"></script>
+        {/* Monetag Standalone Vignette Banner (100% No Popunders!) */}
+        <script src="https://n6wxm.com/vignette.min.js" data-zone="11424905" async data-cfasync="false"></script>
       </head>
       <body>
         {children}
