2e867e604d54d9ffa695a51beeca7c1d777adeb2
Nazifi Ibrahim <nazifiibrahim003@gmail.com>
2026-07-27 00:32:14 +0100

    feat: replace multitag with standalone in-page push and vignette banner scripts to prevent popunders

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -131,7 +131,9 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
-        <script src="https://quge5.com/88/tag.min.js" data-zone="264153" async data-cfasync="false"></script>
+        {/* Monetag In-Page Push & Vignette Banners (No Popunders!) */}
+        <script src="https://quge5.com/88/tag.min.js" data-zone="11424164" async data-cfasync="false"></script>
+        <script src="https://quge5.com/88/tag.min.js" data-zone="11424165" async data-cfasync="false"></script>
       </head>
       <body>
         {children}
